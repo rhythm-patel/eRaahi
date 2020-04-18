213a3ef39cc0c5ba6403e05225b34d40ffdf02b6
Aman Kumar Gupta <aman18217@iiitd.ac.in>
2020-04-18 13:52:19 +0530

    Add files via upload

---
+++ AKG/SQL files/locations.sql
@@ -0,0 +1,26 @@
+CREATE TABLE locations(
+   name VARCHAR(100) NOT NULL
+  ,id   INTEGER  NOT NULL PRIMARY KEY AUTO_INCREMENT
+  ,x    INTEGER  NOT NULL
+  ,y    INTEGER  NOT NULL
+);
+INSERT INTO locations(name,id,x,y) VALUES ('Violetmoor',1,100,2);
+INSERT INTO locations(name,id,x,y) VALUES ('Witchshore',2,96,7);
+INSERT INTO locations(name,id,x,y) VALUES ('Newlake',3,92,12);
+INSERT INTO locations(name,id,x,y) VALUES ('Crystalwood',4,88,17);
+INSERT INTO locations(name,id,x,y) VALUES ('Stonegold',5,84,22);
+INSERT INTO locations(name,id,x,y) VALUES ('Lakebush',6,80,27);
+INSERT INTO locations(name,id,x,y) VALUES ('Ostville',7,76,61);
+INSERT INTO locations(name,id,x,y) VALUES ('Shadowmarsh',8,72,59);
+INSERT INTO locations(name,id,x,y) VALUES ('Esterbush',9,23,57);
+INSERT INTO locations(name,id,x,y) VALUES ('Griffinmount',10,25,55);
+INSERT INTO locations(name,id,x,y) VALUES ('Beachcoast',11,27,53);
+INSERT INTO locations(name,id,x,y) VALUES ('Greyholt',12,29,51);
+INSERT INTO locations(name,id,x,y) VALUES ('Ordale',13,31,49);
+INSERT INTO locations(name,id,x,y) VALUES ('Eastsea',14,33,47);
+INSERT INTO locations(name,id,x,y) VALUES ('Riverbridge',15,35,83);
+INSERT INTO locations(name,id,x,y) VALUES ('Merrimill',16,37,86);
+INSERT INTO locations(name,id,x,y) VALUES ('Violetway',17,39,89);
+INSERT INTO locations(name,id,x,y) VALUES ('Waterpond',18,41,92);
+INSERT INTO locations(name,id,x,y) VALUES ('Windhollow',19,43,95);
+INSERT INTO locations(name,id,x,y) VALUES ('Winterwell',20,45,98);
